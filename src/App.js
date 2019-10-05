6c795b477f7d5215ac84e021904bdb8aa9551ea9
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-05 15:35:44 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_BACKEND_URL + '/api/v1/snacks')
+      .then(data => data.json())
+      .then(data => this.setState({
+        data: data
+      }))
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    const { data } = this.state;
+
+    if (data.food) {
+      return (
+        <ul>
+        {
+          this.state.data.food.map((snack, i) => {
+            console.log('snack', snack)
+            return <li key={i}>{snack.item}</li>;
+          })
+        }
+        </ul>
+      )     
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Here is my front-end-app</p>
+            <p>{this.state.data[0]}</p>
+
+          </header>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
